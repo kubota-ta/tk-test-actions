24d2198e9f8593e6b24c25e641c757bfbb2b3644
久保田 健雄 <kubota-takeo@dmm.com>
2023-05-18 05:18:37 +0000

    apply用の設定を追加

--- demo-ec2.tf
+++ demo-ec2.tf
@@ -1,10 +1,10 @@
-resource "aws_instance" "demo-ec2" { 
-    ami = "ami-08928044842b396f0" 
-    instance_type = "t３.micro" 
-    subnet_id = "subnet-55676d23" 
- 
-    tags = { 
-        Name = "demo-ec2-01" 
-    } 
- 
+resource "aws_instance" "demo-ec2" {
+  ami           = "ami-08928044842b396f0"
+  instance_type = "t３.micro"
+  subnet_id     = "subnet-55676d23"
+
+  tags = {
+    Name = "demo-ec2-01"
+  }
+
 }
--- terraform.tf
+++ terraform.tf
@@ -1,7 +1,7 @@
-terraform { 
-  backend "s3" { 
-    bucket = "kubotat-test-actions" 
-    key    = "terraform.tfstate" 
-    region = "ap-northeast-1" 
-  } 
+terraform {
+  backend "s3" {
+    bucket = "kubotat-test-actions"
+    key    = "terraform.tfstate"
+    region = "ap-northeast-1"
+  }
 }
